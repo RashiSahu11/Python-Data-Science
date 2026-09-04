d391f7881a137fa282c8fdfbd9f48f58521cb63e
rashi <rashi11sahu@gmail.com>
2026-09-04 15:50:11 +0530

    ai

---
+++ face_detection.py
@@ -0,0 +1,33 @@
+import cv2
+
+cap=cv2.VideoCapture(1)
+#load model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+
+                                     "haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+
+                                    "haarcascade_eye.xml")
+
+while True:
+    isOpen,frame=cap.read()
+
+    if not isOpen:
+        break
+
+    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_frame,
+                                          scaleFactor=1.1, minNeighbors=5)
+    eyes = eye_cascade.detectMultiScale(gray_frame,
+                                        scaleFactor=1.1,minNeighbors=5)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+    for(ex,ey,ew,eh) in eyes:
+        cv2.rectangle(frame,(ex,ey),(ex+ew, ey+eh),(0,0,255),2)
+
+    cv2.imshow("Face Detection",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()                
